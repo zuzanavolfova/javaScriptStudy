4dfb5a091e96c4e81a87dd858ca73f86bc1d698f
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-04-22 17:01:57 +0200

    find missing elements

--- script2025.js
+++ script2025.js
@@ -121,9 +121,9 @@ function caesarCipher(s, k){
             }
         }   
     }
-    insertionSort1(101, [2, 4, 8, 12, 15, 19, 21, 24, 26, 29, 30, 32, 35, 36, 41, 44, 49, 52, 57, 58, 59, 64, 65, 68, 73, 77, 80, 82, 85, 88, 93, 97, 101, 105, 108, 111, 115, 118, 122, 127, 130, 131, 132, 134, 135, 136, 138, 141, 144, 146, 151, 153, 158, 160, 165, 169, 171, 176, 179, 184, 187, 190, 194, 197, 200, 205, 210, 215, 217, 222, 225, 230, 231, 236, 239, 243, 244, 246, 248, 253, 254, 256, 258, 262, 263, 267, 272, 274, 277, 280, 284, 285, 289, 291, 295, 297, 301, 305, 310, 312, 279])
+    // insertionSort1(101, [2, 4, 8, 12, 15, 19, 21, 24, 26, 29, 30, 32, 35, 36, 41, 44, 49, 52, 57, 58, 59, 64, 65, 68, 73, 77, 80, 82, 85, 88, 93, 97, 101, 105, 108, 111, 115, 118, 122, 127, 130, 131, 132, 134, 135, 136, 138, 141, 144, 146, 151, 153, 158, 160, 165, 169, 171, 176, 179, 184, 187, 190, 194, 197, 200, 205, 210, 215, 217, 222, 225, 230, 231, 236, 239, 243, 244, 246, 248, 253, 254, 256, 258, 262, 263, 267, 272, 274, 277, 280, 284, 285, 289, 291, 295, 297, 301, 305, 310, 312, 279])
     
-//TODO search position of sum
+//TODO search position of sum ------------------------------------------------------------------------------------------------------------------
 // get total summary and array of numbers. Return position of numbers which sum is equal to summary.
     function icecreamParlor(m, arr) {
         let result = [];
@@ -137,4 +137,20 @@ function caesarCipher(s, k){
         return result
     }
 
-    icecreamParlor(4, [1,4,5,3,2])
+    // icecreamParlor(4, [1,4,5,3,2])
+
+// TODO Given two arrays of integers, find which elements in the second array are missing from the first array. ------------------------------------------
+    const arrayA = [3696, 3739, 3714, 3719, 3687, 3732 ]
+    const arrayB = [3697, 3674, 3700, 3652, 3702, 3718, 3693, 3724, 3676, 3657, 3656, 3654, 3721, 3683]
+    function  missingNumbers(arr, brr){
+        for(let i = 0; i <arr.length; i++){
+            let isInArray = brr.includes(arr[i])
+            if(isInArray){
+                brr.splice(brr.indexOf(arr[i]),1)
+            }
+        }
+    let result = [...new Set(brr.sort(function(a,b){return a - b}))]
+    // console.log(result)
+    return result
+    }
+    // missingNumbers(arrayA, arrayB)
