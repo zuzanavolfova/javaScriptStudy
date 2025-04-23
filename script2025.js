fa74c6a840ee0e313059378125bdc990db9540e8
Zuzana Volfova <zuzka.volfova@gmail.com>
2025-04-23 14:32:12 +0200

    object keys function

--- script2025.js
+++ script2025.js
@@ -197,3 +197,21 @@ function caesarCipher(s, k){
     }
 
     // isPangram("We promptly judged antique ivory buckles for the next prize")
+    
+    // TODO print the values of the array ----------------------------------------------------------------------------
+    const shoppingCartArray = [
+        { name: 'banana', qty: 2 },
+        { name: 'apple', qty: 5 },
+        { name: 'cherry', qty: 0 },
+      ];
+      for (let i = 0; i < shoppingCartArray.length; i++) {
+        // console.log(shoppingCartArray[i].name, shoppingCartArray[i].qty);
+      } 
+    const shoppingCartArray2 = [
+        { key: 'banana', val: 2 },
+        { apple: 'banana', val: 5 },
+        { cherry: 'banana', val: 0 },
+      ];
+      for (let i = 0; i < shoppingCartArray.length; i++) {
+        // console.log(shoppingCartArray2[i][Object.keys(shoppingCartArray2[i])[0]], shoppingCartArray2[i][Object.keys(shoppingCartArray2[i])[1]]);
+      }
